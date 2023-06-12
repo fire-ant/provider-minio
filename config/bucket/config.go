9f5178f4e4eba14e23c4261402896e101f3fff42
clavery <chris.lavery@weave.works>
2023-06-12 08:49:25 +0100

    fix linter

--- config/bucket/config.go
+++ config/bucket/config.go
@@ -2,11 +2,15 @@ package bucket
 
 import "github.com/upbound/upjet/pkg/config"
 
+const (
+	bucket = "bucket"
+)
+
 // Configure configures individual resources by adding custom ResourceConfigurators.
 func Configure(p *config.Provider) {
 	p.AddResourceConfigurator("minio_s3_bucket", func(r *config.Resource) {
 		r.ExternalName = config.NameAsIdentifier
-		r.ShortGroup = "bucket"
+		r.ShortGroup = bucket
 		r.ExternalName.SetIdentifierArgumentFn = func(base map[string]any, externalName string) {
 			base["bucket"] = externalName
 		}
@@ -17,7 +21,7 @@ func Configure(p *config.Provider) {
 	})
 	p.AddResourceConfigurator("minio_s3_bucket_notification", func(r *config.Resource) {
 		r.ExternalName = config.NameAsIdentifier
-		r.ShortGroup = "bucket"
+		r.ShortGroup = bucket
 		r.ExternalName.SetIdentifierArgumentFn = func(base map[string]any, externalName string) {
 			base["bucket"] = externalName
 		}
@@ -27,7 +31,7 @@ func Configure(p *config.Provider) {
 	})
 	p.AddResourceConfigurator("minio_s3_bucket_policy", func(r *config.Resource) {
 		r.ExternalName = config.NameAsIdentifier
-		r.ShortGroup = "bucket"
+		r.ShortGroup = bucket
 		r.ExternalName.SetIdentifierArgumentFn = func(base map[string]any, externalName string) {
 			base["bucket"] = externalName
 		}
@@ -37,7 +41,7 @@ func Configure(p *config.Provider) {
 	})
 	p.AddResourceConfigurator("minio_s3_bucket_versioning", func(r *config.Resource) {
 		r.ExternalName = config.NameAsIdentifier
-		r.ShortGroup = "bucket"
+		r.ShortGroup = bucket
 		r.ExternalName.SetIdentifierArgumentFn = func(base map[string]any, externalName string) {
 			base["bucket"] = externalName
 		}
@@ -47,7 +51,7 @@ func Configure(p *config.Provider) {
 	})
 	p.AddResourceConfigurator("minio_s3_object", func(r *config.Resource) {
 		r.ExternalName = config.NameAsIdentifier
-		r.ShortGroup = "bucket"
+		r.ShortGroup = bucket
 		r.ExternalName.SetIdentifierArgumentFn = func(base map[string]any, externalName string) {
 			base["bucket_name"] = externalName
 		}
